e9493d781c37c77e88feaa0ba6ddbbbb7548c570
post9964@naver.com <post9964@naver.com>
2020-10-01 13:33:05 +0900

    SW_Expert_Academy_Level1 (201001)

--- 001_거꾸로출력해보아요/소스.cpp
+++ 001_거꾸로출력해보아요/소스.cpp
@@ -1,1 +1,11 @@
-//test
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a;
+	cin >> a;
+	for (int i = a; i >= 0; i--) {
+		cout << i << " ";
+	}
+}
---
+++ 002_더블더블/소스.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+	int a;
+	cin >> a;
+	
+	for (int i = 0; i <= a; i++) {
+		cout << pow(2, i) << " ";
+	}
+}
